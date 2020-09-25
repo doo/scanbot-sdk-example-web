d11d79cb6a72afff859d98102bfd6b05d9d64339
Nikituh <aare.undo@gmail.com>
2020-09-25 16:37:13 +0200

    basic document preview popup

--- plain-js/main.js
+++ plain-js/main.js
@@ -5,10 +5,17 @@ const licenseKey = "";
 let scanbotSDK;
 let documentScanner;
 
-/** For a smoother experience, wait for everything else to be loaded before loading anything in the SDK */
+// For a smoother experience, wait for everything else to be loaded before loading anything in the SDK
 window.onload = async function() {
+
+    document.getElementById("popup-close-button").onclick = function () {
+        console.log("asfdag");
+        document.getElementById("image-modal").classList.toggle("modal");
+    };
 	scanbotSDK = await ScanbotSDK.initialize({
+        // Required parameter licenseKey
         licenseKey: licenseKey,
+        // Optional: configure another source for the engine
         // engine: "lib/"
     });
 
@@ -16,6 +23,8 @@ window.onload = async function() {
     	containerId: containerId,
         onDocumentDetected: e => {
             console.log("Detected Document:", e);
+            document.getElementById("document-image").src = e.cropped ? e.cropped : e.original;
+            document.getElementById("image-modal").classList.toggle("modal");
         },
         onError: e => {
             console.log("Error:", e);
