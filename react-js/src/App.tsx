b78b630a8b54b9021e24563f7579c98d52e0ad0f
Nikituh <aare.undo@gmail.com>
2020-09-25 10:52:07 +0200

    recreated react example with tsx; basic scanbot sdk implementation

---
+++ react-js/src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+
+import ScanbotSDK from "scanbot-web-sdk/webpack";
+import {DocumentScannerConfiguration} from "scanbot-web-sdk/component/model/document-scanner-configuration";
+
+export default class App extends React.Component<any, any> {
+
+    SCANNER_CONTAINER = "document-scanner-view";
+    license = "";
+    SDK: ScanbotSDK | undefined;
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    async componentDidMount() {
+        this.SDK = await ScanbotSDK.initialize({
+            licenseKey: this.license,
+            engine: "/"
+        });
+
+        if (!this.SDK.initialized) {
+            const info = await this.SDK.getLicenseInfo();
+            console.log("Something went wrong. Here's your license info:", info);
+            return;
+        }
+
+        const configuration: DocumentScannerConfiguration = {
+            onDocumentDetected: async (result: any) => {
+                console.log("Detected document:", result);
+            },
+            containerId: this.SCANNER_CONTAINER
+        };
+
+        await this.SDK.createDocumentScanner(configuration);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div id={this.SCANNER_CONTAINER} style={this.props.style}/>
+            </div>
+        );
+    }
+}
