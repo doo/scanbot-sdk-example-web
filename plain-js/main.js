635b6f0113a7f9df1cfbfd76abd9a1cd8c67d71d
Nikituh <aare.undo@gmail.com>
2020-09-30 14:24:10 +0200

    plain-js: styling fixes and additional comments

--- plain-js/main.js
+++ plain-js/main.js
@@ -9,7 +9,6 @@ let documentScanner;
 window.onload = async function() {
 
     document.getElementById("popup-close-button").onclick = function () {
-        console.log("asfdag");
         document.getElementById("image-modal").classList.toggle("modal");
     };
 	scanbotSDK = await ScanbotSDK.initialize({
@@ -20,10 +19,12 @@ window.onload = async function() {
     });
 
     const config = {
+        // containerId is the only required parameter. It is the parent object of the Document Scanner
     	containerId: containerId,
         onDocumentDetected: e => {
+    	    // If a document was detected (whether manual or automatic detection), onDocumentDetected callback will trigger
             console.log("Detected Document:", e);
-            // If a document was found, 'cropped' will contain its base64 representation.
+            // If a document was found, 'cropped' will contain its cropped out base64 representation.
             // 'original' is the image you snapped, that always contains the image you took
             document.getElementById("document-image").src = e.cropped ? e.cropped : e.original;
             document.getElementById("image-modal").classList.toggle("modal");
