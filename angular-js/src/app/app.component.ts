0e6bc13c63e9a1c24157d17aeb25d298112afdd5
Nikituh <aare.undo@gmail.com>
2020-10-01 12:10:58 +0200

    angular scanbotsdk initialization logic, styles cleanup

--- angular-js/src/app/app.component.ts
+++ angular-js/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import ScanbotSDK from "scanbot-web-sdk/component/scanbot-sdk";
+import {InitializationOptions} from "scanbot-web-sdk/component/model/initialization-options";
+import {DocumentScannerConfiguration} from "scanbot-web-sdk/component/model/document-scanner-configuration";
+import "scanbot-web-sdk/component/styles/main.css";
+import "scanbot-web-sdk/component/styles/shutter.css";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-js';
+
+  SDK: ScanbotSDK;
+  constructor() {
+    console.log("constructed")
+  }
+
+  async ngOnInit() {
+    const options: InitializationOptions = {
+      // License key is a required parameter, leave empty for trial
+      licenseKey: ""
+      // Optional: configure another source for the engine
+      // engine: "lib/"
+    };
+    this.SDK = await ScanbotSDK.initialize(options);
+
+    const configuration: DocumentScannerConfiguration = {
+      containerId: "scanbot-camera-container"
+    };
+    await this.SDK.createDocumentScanner(configuration);
+  }
+
 }
