0eb9c2f32b718073da37d1b37cfbc79c87088f80
Nikituh <aare.undo@gmail.com>
2021-02-02 18:31:13 +0100

    plain-js: fully-fledged example project

---
+++ plain-js/js/config.js
@@ -0,0 +1,14 @@
+
+class Config {
+    static license() {
+        return "";
+    }
+
+    static scannerContainerId() {
+        return "scanbot-camera-container";
+    }
+
+    static croppingViewContainerId() {
+        return "cropping-view-container";
+    }
+}
---
+++ plain-js/js/view-utils.js
@@ -0,0 +1,30 @@
+
+class ViewUtils {
+
+    static showLoading() {
+        Utils.getElementByClassName("loading-screen").style.display = "block";
+    }
+    static hideLoading() {
+        Utils.getElementByClassName("loading-screen").style.display = "none";
+    }
+
+    static flash() {
+        const flash = Utils.getElementByClassName("flash");
+        flash.style.display = "block";
+
+        this.animateFlashOpacity(0.5, () => {
+            this.animateFlashOpacity(0.0, () => {
+                flash.style.opacity = 1.0;
+                flash.style.display = "none";
+            })
+        });
+    }
+
+    static animateFlashOpacity(opacity, complete) {
+        const flash = Utils.getElementByClassName("flash");
+        setTimeout(() => {
+            flash.style.opacity = opacity;
+            complete();
+        }, 150);
+    }
+}
