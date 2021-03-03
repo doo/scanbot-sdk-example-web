00afac13746c9ac771c636267350e55e71358f01
Nikituh <aare.undo@gmail.com>
2021-03-03 11:57:56 +0100

    basic 'push view controller' animation'

--- react-js/src/pages/document-scanner-page.tsx
+++ react-js/src/pages/document-scanner-page.tsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import {ScanbotSdkService} from "../service/scanbot-sdk-service";
+import styled, {keyframes} from "styled-components";
 
 export default class DocumentScannerPage extends React.Component<any, any>{
 
@@ -14,10 +15,16 @@ export default class DocumentScannerPage extends React.Component<any, any>{
     }
 
     render() {
+        console.log("size", window.innerWidth, window.innerHeight);
+        const animate = keyframes`
+            from {transform: translateX(100%); } 
+            to   {transform: translateX(0%); }
+        `;
+        const Push = styled.div`animation: ${animate} 1s;`;
         return (
-            <div style={{height: "100%"}}>
-                <div id={ScanbotSdkService.DOCUMENT_SCANNER_CONTAINER} style={{width: "100%", height: "100%"}}/>
-            </div>
+            <Push style={{height: "100%"}}>
+                    <div id={ScanbotSdkService.DOCUMENT_SCANNER_CONTAINER} style={{width: "100%", height: "100%"}}/>
+            </Push>
         );
     }
 }
