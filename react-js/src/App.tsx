ee8d36a8b2c4cced2583dc849f12d71667885501
Nikituh <aare.undo@gmail.com>
2021-06-07 14:56:01 +0300

    updated ScanbotSDK; commented out onboarding

--- react-js/src/App.tsx
+++ react-js/src/App.tsx
@@ -86,11 +86,11 @@ export default class App extends React.Component<any, any> {
 
     render() {
 
-    	if (this.state.shouldShowOnboarding) {
-			return <Onboarding skip={() => {
-				this.setState({shouldShowOnboarding: false});
-			}} />;
-		}
+    	// if (this.state.shouldShowOnboarding) {
+		// 	return <Onboarding skip={() => {
+		// 		this.setState({shouldShowOnboarding: false});
+		// 	}} />;
+		// }
 
         return (
             <div>
