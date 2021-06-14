e70e3e3b359b1704f6d93811bd8afed0201fb51f
Nikituh <aare.undo@gmail.com>
2021-06-14 18:48:07 +0300

    refactored menu to class components

--- react-js/src/pages/main-menu/main-menu-section.jsx
+++ react-js/src/pages/main-menu/main-menu-section.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
 import './main-menu-section-styles.scss'
 
-const Section = ({icon, title, cards, links}) => {
+export default class Section extends React.Component {
 
-    return (
-        <div className='sectionContainer'>
-            <div className='titleContainer'>
-                <img src={icon} alt="icon" className='icon'/>
-                <h2 className='title'>{title}</h2>
-            </div>
-            {cards && <div className='cardsContainer'>
-                {cards.map((card, index) => {
-                const {image, title, description, onclick} = card
-                    return (
-                        <div key={index} className='card' onClick={onclick}>
-                            <div><img src={image} alt="card illustration" className='image'/></div>
-                            <div>
-                                <h3 className='title'>{title}</h3>
-                                <p className='description'>{description}</p>
+    render() {
+        const {icon, title, cards, links} = this.props;
+        return (
+            <div className='sectionContainer'>
+                <div className='titleContainer'>
+                    <img src={icon} alt="icon" className='icon'/>
+                    <h2 className='title'>{title}</h2>
+                </div>
+                {cards && <div className='cardsContainer'>
+                    {cards.map((card, index) => {
+                        const {image, title, description, onclick} = card;
+                        return (
+                            <div key={index} className='card' onClick={onclick}>
+                                <div><img src={image} alt="card illustration" className='image'/></div>
+                                <div>
+                                    <h3 className='title'>{title}</h3>
+                                    <p className='description'>{description}</p>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
-            </div>}
-            {links && <div className='linksContainer'>
-                {links.map((link, index) => {
-                const {text, onclick} = link
-                    return (
-                        <a key={index} href={onclick} className='link'>{text}</a>
-                    )
-                })}
-            </div>}
-        </div>
-    )
+                        )
+                    })}
+                </div>}
+                {links && <div className='linksContainer'>
+                    {links.map((link, index) => {
+                        const {text, onclick} = link;
+                        return (
+                            <a key={index} href={onclick} className='link'>{text}</a>
+                        )
+                    })}
+                </div>}
+            </div>
+        )
+    }
 }
-
-export default Section
--- react-js/src/pages/main-menu/main-menu.jsx
+++ react-js/src/pages/main-menu/main-menu.jsx
@@ -5,47 +5,53 @@ import Section from './main-menu-section'
 import {sectionContent, cardContent, linkContent} from './main-menu-model'
 
 
-const MainMenu = ({language, callDocument, callBarcode, viewDocuments}) => {
-    return (
-        <>
-            <div className='logoContainer'>
-                <img
-                    className='logo'
-                    src={require('../../assets/ScanbotSDKwhite.svg')}
-                    alt='Scanbot logo'
-                />
-            </div>
-            <div className='contentContainer'>
-                <Section {...sectionContent(language).documentScanner} 
-                    cards={[
-                        {...cardContent(language).scanDocuments, onclick: callDocument},
-                        {...cardContent(language).viewDocuments, onclick: viewDocuments},
-                    ]}
-                />
-                <Section {...sectionContent(language).dataDetectors} 
-                    cards={[
-                        {...cardContent(language).barcode, onclick: callBarcode},
-                    ]}
-                />
-                <Section {...sectionContent(language).about}
-                    links={[
-                        {...linkContent(language).learnMore}
-                    ]}
-                />
-                <Section {...sectionContent(language).legal}
-                    links={[
-                        {...linkContent(language).terms},
-                        {...linkContent(language).privacy},
-                        {...linkContent(language).imprint},
-                        {...linkContent(language).acknowledgements}
-                    ]}
-                />
-                <div className='footer'>
-                    <p dangerouslySetInnerHTML={{ __html: `App version 1.4.0.60<br />Scanbot SDK version 1.78.0<br />© 2020 doo GmbH. All rights reserved.` }}/>
+export default class MainMenu extends React.Component {
+    
+    render() {
+        const {language, callDocument, callBarcode, viewDocuments} = this.props;
+        return (
+            <>
+                <div className='logoContainer'>
+                    <img
+                        className='logo'
+                        src={require('../../assets/ScanbotSDKwhite.svg')}
+                        alt='Scanbot logo'
+                    />
                 </div>
-            </div>
-        </>
-    )
+                <div className='contentContainer'>
+                    <Section {...sectionContent(language).documentScanner}
+                             cards={[
+                                 {...cardContent(language).scanDocuments, onclick: callDocument},
+                                 {...cardContent(language).viewDocuments, onclick: viewDocuments},
+                             ]}
+                    />
+                    <Section {...sectionContent(language).dataDetectors}
+                             cards={[
+                                 {...cardContent(language).barcode, onclick: callBarcode},
+                             ]}
+                    />
+                    <Section {...sectionContent(language).about}
+                             links={[
+                                 {...linkContent(language).learnMore}
+                             ]}
+                    />
+                    <Section {...sectionContent(language).legal}
+                             links={[
+                                 {...linkContent(language).terms},
+                                 {...linkContent(language).privacy},
+                                 {...linkContent(language).imprint},
+                                 {...linkContent(language).acknowledgements}
+                             ]}
+                    />
+                    <div className='footer'>
+                        <p dangerouslySetInnerHTML={{__html:
+                                `App version 1.4.0.60<br />
+                                 Scanbot SDK version 1.78.0<br />
+                                 © 2020 doo GmbH. All rights reserved.`
+                        }}/>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
-
-export default MainMenu
