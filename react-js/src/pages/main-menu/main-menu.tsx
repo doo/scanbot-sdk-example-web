27105a74e1ccd91159a55ac1f340f14ca903ac57
spaceDolph1n <tiagojsr1@gmail.com>
2021-06-15 15:30:14 +0200

    header and footer components

--- react-js/src/pages/main-menu/main-menu.tsx
+++ react-js/src/pages/main-menu/main-menu.tsx
@@ -4,31 +4,53 @@ import './main-menu-styles.scss'
 import Section from './main-menu-section'
 import {sectionContent} from './main-menu-model'
 
+class Header extends React.Component<any, any> {
+	render() {
+		return (
+            <div className='logoContainer'>
+                <img
+                    className='logo'
+                    src={require('../../assets/ScanbotSDKwhite.svg')}
+                    alt='Scanbot logo'
+                />
+            </div>
+        )
+	}
+}
 
+class Footer extends React.Component<any, any> {
+	render() {
+		return (
+            <div className='footer'>
+                <p dangerouslySetInnerHTML={{__html:
+                        `App version 1.4.0.60<br />
+                        Scanbot SDK version 1.78.0<br />
+                        © 2020 doo GmbH. All rights reserved.`
+                }}/>
+                <p dangerouslySetInnerHTML={{__html: this.props.language === 'de' 
+                    ? `App version 1.4.0.60<br />
+                    Scanbot SDK version 1.78.0<br />
+                    © 2020 doo GmbH. Alle Rechte vorbehalten.`
+                    : `App version 1.4.0.60<br />
+                    Scanbot SDK version 1.78.0<br />
+                    © 2020 doo GmbH. All rights reserved.`
+                }}/>
+            </div>
+        )
+	}
+}
 export default class MainMenu extends React.Component<any, any> {
 
     render() {
         return (
             <>
-                <div className='logoContainer'>
-                    <img
-                        className='logo'
-                        src={require('../../assets/ScanbotSDKwhite.svg')}
-                        alt='Scanbot logo'
-                    />
-                </div>
+                <Header />
                 <div className='contentContainer'>
                     <Section {...sectionContent({...this.props}).documentScanner} />
                     <Section {...sectionContent({...this.props}).dataDetectors} />
                     <Section {...sectionContent({...this.props}).about} />
                     <Section {...sectionContent({...this.props}).legal} />
-                    <div className='footer'>
-                        <p dangerouslySetInnerHTML={{__html:
-                                `App version 1.4.0.60<br />
-                                 Scanbot SDK version 1.78.0<br />
-                                 © 2020 doo GmbH. All rights reserved.`
-                        }}/>
-                    </div>
+                    <Footer language={this.props.language} />
                 </div>
             </>
         )
