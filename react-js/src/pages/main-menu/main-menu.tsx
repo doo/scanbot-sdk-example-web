a64136c7e818859e88f95854a41da0e6fbbbdb71
spaceDolph1n <tiagojsr1@gmail.com>
2021-06-15 16:04:12 +0200

    storage service show alert and alert component

--- react-js/src/pages/main-menu/main-menu.tsx
+++ react-js/src/pages/main-menu/main-menu.tsx
@@ -3,6 +3,7 @@ import './main-menu-styles.scss'
 
 import Section from './main-menu-section'
 import {sectionContent} from './main-menu-model'
+import {StorageService} from "../../service/storage-service";
 
 class Header extends React.Component<any, any> {
 	render() {
@@ -22,11 +23,6 @@ class Footer extends React.Component<any, any> {
 	render() {
 		return (
             <div className='footer'>
-                <p dangerouslySetInnerHTML={{__html:
-                        `App version 1.4.0.60<br />
-                        Scanbot SDK version 1.78.0<br />
-                        © 2020 doo GmbH. All rights reserved.`
-                }}/>
                 <p dangerouslySetInnerHTML={{__html: this.props.language === 'de' 
                     ? `App version 1.4.0.60<br />
                     Scanbot SDK version 1.78.0<br />
@@ -39,18 +35,43 @@ class Footer extends React.Component<any, any> {
         )
 	}
 }
+
+class Alert extends React.Component<any, any> {
+	render() {
+        console.log('here')
+        const {language} = this.props
+		return (
+            <div className='alert'>
+                <p>{language === 'de'
+                    ? 'Dürfen wir anonyme Absturzberichte erstellen, um die Benutzererfahrung zu verbessern?'
+                    : 'Can we create anonymous crash reports, helping us improve your user experience?'
+                }</p>
+                <button>{language === 'de' ? 'ABLEHNEN' : 'DENY'}</button>
+                <button>{language === 'de' ? 'ERLAUBEN' : 'ALLOW'}</button>
+            </div>
+        )
+	}
+}
 export default class MainMenu extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            showAlert: StorageService.instance.getShowAlert()
+        };
+    }
 
     render() {
+        const {language} = this.props
         return (
             <>
                 <Header />
+                {this.state.showAlert && <Alert language={language}/>}
                 <div className='contentContainer'>
                     <Section {...sectionContent({...this.props}).documentScanner} />
                     <Section {...sectionContent({...this.props}).dataDetectors} />
                     <Section {...sectionContent({...this.props}).about} />
                     <Section {...sectionContent({...this.props}).legal} />
-                    <Footer language={this.props.language} />
+                    <Footer language={language} />
                 </div>
             </>
         )
