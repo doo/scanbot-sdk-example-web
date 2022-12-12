33f9b6d4cb9324050be5c4eb8433b74ccb67aac3
Rutkay Biyik <rutkay@scanbot.io>
2022-12-12 09:52:16 +0100

    Plainjs mrz detection undefined checks

--- plain-js/main.js
+++ plain-js/main.js
@@ -433,14 +433,14 @@ async function onMrzDetected(mrz) {
 
   let text = "";
   if (mrz) {
-    text = text + 'Document Type: ' + mrz.documentType.value + ` (${Number(mrz.documentType.confidence).toFixed(3)})` + '\n';
-    text = text + 'First Name: ' + mrz.givenNames.value + ` (${Number(mrz.givenNames.confidence).toFixed(3)})` + '\n';
-    text = text + 'Last Name: ' + mrz.surname.value + ` (${Number(mrz.surname.confidence).toFixed(3)})` + '\n';
-    text = text + 'Issuing Authority: ' + mrz.issuingAuthority.value + ` (${Number(mrz.issuingAuthority.confidence).toFixed(3)})` + '\n';
-    text = text + 'Nationality: ' + mrz.nationality.value + ` (${Number(mrz.nationality.confidence).toFixed(3)})` + '\n';
-    text = text + 'Birth Date: ' + mrz.birthDate.value + ` (${Number(mrz.birthDate.confidence).toFixed(3)})` + '\n';
-    text = text + 'Gender: ' + mrz.gender.value + ` (${Number(mrz.gender.confidence).toFixed(3)})` + '\n';
-    text = text + 'Date of Expiry: ' + mrz.expiryDate.value + ` (${Number(mrz.expiryDate.confidence).toFixed(3)})` + '\n';
+    text = text + 'Document Type: ' + (mrz.documentType ? (mrz.documentType.value + ` (${Number(mrz.documentType.confidence).toFixed(3)})`) : '') + '\n';
+    text = text + 'First Name: ' + (mrz.givenNames ? (mrz.givenNames.value + ` (${Number(mrz.givenNames.confidence).toFixed(3)})`) : '') + '\n';
+    text = text + 'Last Name: ' + (mrz.surname ? (mrz.surname.value + ` (${Number(mrz.surname.confidence).toFixed(3)})`) : '') + '\n';
+    text = text + 'Issuing Authority: ' + (mrz.issuingAuthority ? (mrz.issuingAuthority.value + ` (${Number(mrz.issuingAuthority.confidence).toFixed(3)})`) : '') + '\n';
+    text = text + 'Nationality: ' + (mrz.nationality ? (mrz.nationality.value + ` (${Number(mrz.nationality.confidence).toFixed(3)})`) : '') + '\n';
+    text = text + 'Birth Date: ' + (mrz.birthDate ? (mrz.birthDate.value + ` (${Number(mrz.birthDate.confidence).toFixed(3)})`) : '') + '\n';
+    text = text + 'Gender: ' + (mrz.gender ? (mrz.gender.value + ` (${Number(mrz.gender.confidence).toFixed(3)})`) : '') + '\n';
+    text = text + 'Date of Expiry: ' + (mrz.expiryDate ? (mrz.expiryDate.value + ` (${Number(mrz.expiryDate.confidence).toFixed(3)})`) : '') + '\n';
   }
 
   alert(text);
