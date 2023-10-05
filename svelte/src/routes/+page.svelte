2e732c41da798df8ae7239393ea87cb4ae1ba561
Aare Undo <aare.undo@gmail.com>
2023-10-05 10:34:59 +0300

    svelte: projet skeleton

---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import { onMount } from 'svelte';
+    onMount(async () => {
+		// const sdk = (await import('scanbot-web-sdk')).default;
+		// const scanbotSDK = await sdk.initialize({ licenseKey: '' });
+        console.log('Hello from onMount!');
+	});
+</script>
