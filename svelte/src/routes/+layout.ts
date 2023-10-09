d13deb0feab7787f3566264f3d2a5b0156363603
Aare Undo <aare.undo@gmail.com>
2023-10-09 15:57:25 +0300

    svelte: disable ssr

---
+++ svelte/src/routes/+layout.ts
@@ -0,0 +1,7 @@
+
+/**
+ * Disable SSR.
+ * For most use-cases, it makes sense to initialize ScanbotSDK in a client-side rendered application
+ * As such, it can be initialized on page load and used throughout the application
+ */
+export const ssr = false;
