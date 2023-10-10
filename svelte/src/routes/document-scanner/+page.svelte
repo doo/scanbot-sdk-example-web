48d8b26fbada85c68463a4770dc7741d0031009c
Aare Undo <aare.undo@gmail.com>
2023-10-10 12:38:58 +0300

    svelte: document scanner initialization and configuration

--- svelte/src/routes/document-scanner/+page.svelte
+++ svelte/src/routes/document-scanner/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import ScanbotSDKService from '../../service/scanbot-sdk-service';
 	import Header from '../../subviews/Header.svelte';
 	onMount(async () => {
-		// ScanbotSDKService.instance.initialize();
+		ScanbotSDKService.instance.createDocumentScanner('document-scanner');
 	});
-
-	console.log('document-scanner +page.svelte');
+	onDestroy(() => {
+		ScanbotSDKService.instance.disposeDocumentScanner();
+	});
 </script>
 
 <Header title="Document Scanner" isBackButtonVisible={true} />
+
+<div id="document-scanner" style="width: 100%; height: calc(100vh - 50px); background-color: black" />
--- svelte/src/service/scanbot-sdk-service.ts
+++ svelte/src/service/scanbot-sdk-service.ts
@@ -1,21 +1,44 @@
 
 import type ScanbotSDK from 'scanbot-web-sdk';
 import type { IBarcodeScannerHandle } from 'scanbot-web-sdk/@types/interfaces/i-barcode-scanner-handle';
+import type { IDocumentScannerHandle } from 'scanbot-web-sdk/@types/interfaces/i-document-scanner-handle';
 import type { BarcodeResult } from 'scanbot-web-sdk/@types/model/barcode/barcode-result';
 import type { BarcodeScannerConfiguration } from 'scanbot-web-sdk/@types/model/configuration/barcode-scanner-configuration';
+import type { DocumentScannerConfiguration } from 'scanbot-web-sdk/@types/model/configuration/document-scanner-configuration';
+import type { DocumentDetectionResult } from 'scanbot-web-sdk/@types/model/document/document-detection-result';
 
 export default class ScanbotSDKService {
     
     public static instance: ScanbotSDKService = new ScanbotSDKService();
     
     private sdk?: ScanbotSDK;
-    barcodeScanner?: IBarcodeScannerHandle;
+
+    private documentScanner?: IDocumentScannerHandle;
+    private barcodeScanner?: IBarcodeScannerHandle;
 
     public async initialize() {
         // Use dyanic runtime imports to load the SDK else SvelteKit will try to load it before 'window' object is available
         // Also note that initialize is called after a svelte component is mounted
         const sdk = (await import('scanbot-web-sdk')).default;
         this.sdk = await sdk.initialize({ licenseKey: '' });
+    }
+
+    public async createDocumentScanner(containerId: string) {
+        const config: DocumentScannerConfiguration = {
+            containerId : containerId,
+            onDocumentDetected: (e: DocumentDetectionResult) => {
+                console.log("Found document: ", JSON.stringify(e));
+            },
+            onError: (error: Error) => {
+                console.log("Encountered error in DocumentScanner: ", error);
+            }
+
+        };
+        this.documentScanner = await this.sdk?.createDocumentScanner(config);
+    }
+
+    public async disposeDocumentScanner() {
+        this.documentScanner?.dispose();
     }
 
     public async createBarcodeScanner(containerId: string) {
@@ -26,15 +49,14 @@ export default class ScanbotSDKService {
                     widthProportion: 0.8,
                 }
             },
-            onError: (error: unknown) => {
-                console.log("Encountered error in BarcodeScanner: ", error);
-            },
             onBarcodesDetected: (e: BarcodeResult) => {
                 console.log("Found barcode: ", JSON.stringify(e));
-            }
+            },
+            onError: (error: Error) => {
+                console.log("Encountered error in BarcodeScanner: ", error);
+            },
         };
 
-        console.log("sdk", this.sdk);
         this.barcodeScanner = await this.sdk?.createBarcodeScanner(config);
     }
 
