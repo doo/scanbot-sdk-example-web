957c6d500a7795e64d6d2a79c5f51ae3531ca73f
Aare Undo <aare.undo@gmail.com>
2023-10-11 13:44:38 +0300

    svelte: proper hover crop button

--- svelte/src/routes/image-details/+page.svelte
+++ svelte/src/routes/image-details/+page.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 	import ScanbotSDKService from '../../service/scanbot-sdk-service';
 	import Header from '../../subviews/Header.svelte';
-	import Footer from '../../subviews/Footer.svelte';
+	import Crop from '../../subviews/toast/icon/Crop.svelte';
 
 	const id = $page.url.searchParams.get('id');
 	const document = ScanbotSDKService.instance.getDocument(id);
@@ -12,6 +12,15 @@
 
 <div class="image-container">
 	<img class="document-image" src={document?.base64} alt="<document>" />
+</div>
+
+<div class="action-item-container">
+	<button
+		class="no-style crop-button"
+		on:click={() => {
+			console.log('onclick');
+		}}><Crop /></button
+	>
 </div>
 
 <style>
@@ -25,5 +34,23 @@
 		border-radius: 5px;
 		max-width: 100%;
 		vertical-align: middle;
+	}
+    .action-item-container {
+        margin-top: 20px;
+        display: flex;
+        justify-content: center;
+    }
+	.crop-button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 60px;
+		height: 60px;
+		border-radius: 50%;
+		border: none;
+		outline: none;
+		cursor: pointer;
+		background-color: #c8193c;
+        box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
 	}
 </style>
---
+++ svelte/src/subviews/toast/icon/Crop.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let color = "white";
+    export let size = 32;
+
+    const fullSize = 54;
+    const scale = size / fullSize;
+    console.log("scale", scale);
+</script>
+<svg xmlns="http://www.w3.org/2000/svg" height={fullSize} width={fullSize} style="transform: scale({scale})">
+	<path
+		stroke-linejoin="round"
+		stroke={color}
+		stroke-linecap="round"
+		stroke-width="5"
+		fill="none"
+		d="m13.2 39 35-34.6m-45 8.1h36.6v38m-27-47v36.6h38"
+	/>
+</svg>
