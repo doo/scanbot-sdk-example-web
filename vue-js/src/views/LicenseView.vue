e39712011db787f43243194219c1b0e95d8a51b7
Benjamin Klotz <benjamin@scanbot.io>
2023-12-13 17:01:23 +0100

    vue.js pretty print license

--- vue-js/src/views/LicenseView.vue
+++ vue-js/src/views/LicenseView.vue
@@ -1,9 +1,9 @@
 <template>
   <PageLayout title="License Info" :is-loading=false>
     <p class="m-3">
-      License: <br>
-      <pre>{{ text }}</pre>
+      License:
     </p>
+    <p class="m-3" v-html="licenseText"></p>
   </PageLayout>
 </template>
 
@@ -13,13 +13,14 @@ import {inject, ref, onBeforeMount} from "vue";
 import PageLayout from "@/components/PageLayout.vue";
 import type ScanbotSDK from "scanbot-web-sdk";
 
-const text = ref();
+const licenseText = ref();
 
 onBeforeMount(async () => {
-  text.value = "Loading...";
+  licenseText.value = "Loading...";
   const scanbotSDK:ScanbotSDK = await inject("scanbotSDK")!;
   const info = await scanbotSDK.getLicenseInfo();
-  text.value = JSON.stringify(info);
+  licenseText.value = JSON.stringify(info, null, " ");
+  licenseText.value = licenseText.value.replace(/\n/g, "<br/>");
 });
 
 </script>
