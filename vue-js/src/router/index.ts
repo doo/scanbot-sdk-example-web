a9eb77d56eb3024354a52a741a44d02a06917a15
Benjamin Klotz <benjamin@scanbot.io>
2023-12-13 15:05:59 +0100

    vue.js Adapted vue.js router for static deployment EPIC-3983

--- vue-js/src/router/index.ts
+++ vue-js/src/router/index.ts
@@ -1,11 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', redirect: '/index.html' },
     {
-      path: '/',
+      path: '/index.html',
       name: 'home',
       component: HomeView
     },
