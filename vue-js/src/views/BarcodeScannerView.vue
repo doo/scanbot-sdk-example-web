26401e255998d584240b19dbe5e4d84b3456d11e
Benjamin Klotz <benjamin@scanbot.io>
2024-01-31 12:17:28 +0100

    EPIC-4251 Vue.js: Updated list of supported barcodes

--- vue-js/src/views/BarcodeScannerView.vue
+++ vue-js/src/views/BarcodeScannerView.vue
@@ -31,6 +31,7 @@ onBeforeMount(() => {
 })
 onMounted(async () => {
   const barcodeFormats: BarcodeFormat[] = [
+    "ONE_D",
     "AZTEC",
     "CODABAR",
     "CODE_39",
@@ -38,17 +39,28 @@ onMounted(async () => {
     "CODE_128",
     "DATA_MATRIX",
     "EAN_8",
-    "EAN_13",
+    "EAN_13", 
     "ITF",
     "MAXICODE",
     "PDF_417",
     "QR_CODE",
-    "RSS_14",
-    "RSS_EXPANDED",
     "UPC_A",
     "UPC_E",
     "UPC_EAN_EXTENSION",
     "MSI_PLESSEY",
+    "IATA_2_OF_5",
+    "INDUSTRIAL_2_OF_5",
+    "CODE_25",
+    "MICRO_QR_CODE",
+    "USPS_INTELLIGENT_MAIL",
+    "ROYAL_MAIL",
+    "JAPAN_POST",
+    "ROYAL_TNT_POST",
+    "AUSTRALIA_POST",
+    "DATABAR",
+    "DATABAR_EXPANDED",
+    "DATABAR_LIMITED",
+    "GS1_COMPOSITE"
   ];
 
 
