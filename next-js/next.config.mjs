e4c7c250bfac1029b7235d175f292a76518fd9b6
Aare Undo <aare.undo@gmail.com>
2024-02-02 14:50:45 +0200

    next: disable strict mode

--- next-js/next.config.mjs
+++ next-js/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'build',
+    // Disable strict mode because that, by design, calls 'useEffect' twice, only in DEBUG mode
+    // but ScanbotSDK initialization is a resource-consuming operation and we don't want to call it twice even in DEBUG mode
+    reactStrictMode: false
 };
 
 export default nextConfig;
