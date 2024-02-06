f4861b6e236f76e0d264518d28c51f60704b38f8
Aare Undo <aare.undo@gmail.com>
2024-02-06 10:11:30 +0200

    next: define static site build as optional

--- next-js/next.config.mjs
+++ next-js/next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
-    // Disable strict mode because that, by design, calls 'useEffect' twice, only in DEBUG mode
-    // but ScanbotSDK initialization is a resource-consuming operation and we don't want to call it twice even in DEBUG mode
+
+    /**
+     * Uncomment the following two lines and run 'npm run build' to build a static site in the 'build' directory. 
+     * That site can then be deployed anywhere that's able to serve basic html/css/js.
+     */
+    // output: 'export',
+    // distDir: 'build',
+
+    /**
+     * Disable strict mode because that, by design, calls 'useEffect' twice, only in DEBUG mode
+     * but ScanbotSDK initialization is a resource-consuming operation and we don't want to call it twice even in DEBUG mode
+     */
     reactStrictMode: false
 };
 
