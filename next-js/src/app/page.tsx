e0acf5705d451b91615e6723d75b8e951ecc1c82
Aare Undo <aare.undo@gmail.com>
2024-02-14 18:06:52 +0200

    next: sdk loading skeleton blocker

--- next-js/src/app/page.tsx
+++ next-js/src/app/page.tsx
@@ -1,30 +1,39 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "./subviews/header";
 import ListItem from "./subviews/list-item";
 import ScanbotSDKService from "./services/scanbot-sdk-service";
 
 export default function Home() {
 
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
 	useEffect(() => {
-		ScanbotSDKService.instance.initialize();
-	  }, [])
-	
+		async function loadSDK() {
+			await ScanbotSDKService.instance.initialize();
+			setIsLoading(false);
+		}
+		loadSDK();
+	}, [isLoading])
+
 	return (
 		<div style={{ backgroundColor: "#f5f5f5", width: "100%", height: "100vh" }}>
 			<Header />
 			<ListItem
+				isLoading={isLoading}
 				href="/pages/document-scanner"
 				title="Document Scanner"
 				description="Scan any documents in front of you with auto-capture and user guidance."
 			/>
 			<ListItem
+				isLoading={isLoading}
 				href="/pages/barcode-scanner"
 				title="Barcode Scanner"
 				description="Scan any 1D or 2D barcode in front of you within the finder window."
 			/>
 			<ListItem
+				isLoading={isLoading}
 				href="/pages/document-list"
 				title="Document List"
 				description="View the documents you have scanned"
--- next-js/src/app/subviews/list-item.tsx
+++ next-js/src/app/subviews/list-item.tsx
@@ -1,14 +1,17 @@
+
 import Link from "next/link";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function ListItem(props: Readonly<any>) {
     return (
-        <Link href={props.href}>
+        <Link href={props.isLoading ? "/" : props.href}>
             <div style={{ margin: "20px 20px 0 20px", borderBottom: "1px solid lightgray" }}>
                 <div style={{ color: "black", fontSize: "18px", fontWeight: "500", paddingBottom: 5 }}>
-                    {props.title}
+                    {props.isLoading ? <Skeleton/> : props.title}
                 </div>
                 <div style={{ color: "gray", fontSize: "12px", fontWeight: "300", paddingBottom: 12 }}>
-                    {props.description}
+                    {props.isLoading ? <Skeleton count={2}/> : props.description}
                 </div>
             </div>
         </Link>
