520d3aa29ff755329637f45eed4c0b9ed16472e4
Aare Undo <aare.undo@gmail.com>
2024-04-18 16:48:47 +0300

    rtuui: basic scanner start func.; cleanup

--- rtuui/src/App.tsx
+++ rtuui/src/App.tsx
@@ -1,20 +1,26 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { QrCode, QrCode2 } from '@mui/icons-material';
 
-import ScanbotSDK from 'scanbot-web-sdk/webpack/ui';
-import { BarcodeScannerConfiguration } from 'scanbot-web-sdk/@types/ui2/configuration/BarcodeScannerConfiguration';
-import { SingleScanningMode } from 'scanbot-web-sdk/@types/ui2/configuration/SingleScanningModeUseCase';
+import ScanbotSDK from 'scanbot-web-sdk/ui';
+// import ScanbotSDK from 'scanbot-web-sdk';
+
+import BarcodeFormat from 'scanbot-web-sdk/@types/model/barcode/BarcodeFormat';
+// import { SingleScanningMode } from 'scanbot-web-sdk/@types/ui2/configuration/SingleScanningModeUseCase';
+// import { BarcodeScannerConfiguration } from 'scanbot-web-sdk/@types/ui2/configuration/BarcodeScannerConfiguration';
 // import { BarcodeScannerConfiguration, SingleScanningMode } from 'scanbot-web-sdk/@types/ui2/configuration';
 
 function App() {
 
+	const [sdk, setSdk] = useState<ScanbotSDK | null>(null);
+
 	useEffect(() => {
 		async function init() {
 			console.log("Initializing Scanbot SDK");
-			await ScanbotSDK.initialize({ licenseKey: '' });
+			const sdk = await ScanbotSDK.initialize({ licenseKey: '' });
+			setSdk(sdk);
 		}
 		init();
 	}, []);
@@ -24,11 +30,12 @@ function App() {
 			<List>
 				<ListItem disablePadding>
 					<ListItemButton onClick={async () => {
-						// const config = new BarcodeScannerConfiguration();
-						// const useCase = new SingleScanningMode();
-						// config.useCase = useCase;
-						//@ts-ignore
-						const scanResult = await ScanbotSDK.UI.createBarcodeScanner({});
+
+						const config = new ScanbotSDK.UI.Config.BarcodeScannerConfiguration();
+						const useCase = new ScanbotSDK.UI.Config.SingleScanningMode();
+						config.useCase = useCase;
+
+						const scanResult = await ScanbotSDK.UI.createBarcodeScanner(config);
 						console.log("Result: ", scanResult);
 					}}>
 						<ListItemIcon>
--- rtuui/src/main.tsx
+++ rtuui/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import "./main.css"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
