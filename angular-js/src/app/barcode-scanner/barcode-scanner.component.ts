dc3f1ea28f734286b60746b0a9cc7c5db6c484ed
Benjamin Klotz <benjamin@scanbot.io>
2024-08-13 13:36:06 +0200

    Fix error in barcode examples

--- angular-js/src/app/barcode-scanner/barcode-scanner.component.ts
+++ angular-js/src/app/barcode-scanner/barcode-scanner.component.ts
@@ -105,7 +105,7 @@ export class BarcodeScannerComponent implements OnInit {
           // You can override onBarcodeFound and create your own implementation for custom styling, e.g.
           // if you wish to only color in certain types of barcodes, you can find and pick them, as demonstrated below:
           if (code.format === "QR_CODE") {
-            polygon.style({ fill: "rgba(255, 255, 0, 0.3)", stroke: "yellow" })
+            polygon?.style({ fill: "rgba(255, 255, 0, 0.3)", stroke: "yellow" })
           }
         }
       },
