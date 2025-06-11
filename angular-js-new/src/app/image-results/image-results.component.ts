d521f8c4b804cb3ca4e6c705858b52fe23b5da8a
Aare Undo <aare.undo@gmail.com>
2025-06-11 15:28:12 +0200

    angular: allow toast result in image result page

--- angular-js-new/src/app/image-results/image-results.component.ts
+++ angular-js-new/src/app/image-results/image-results.component.ts
@@ -1,7 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ScanbotService } from '../../service/scanbot.service';
 import ScanbotSDK from 'scanbot-web-sdk';
-import { Image } from 'scanbot-web-sdk/@types';
+import { DocumentDetectionResult, DocumentScanningResult, Image } from 'scanbot-web-sdk/@types';
+import { ToastService } from '../../service/toast.service';
+
+class DisplayImage {
+  source?: Image;
+  base64?: string;
+  detectionResult?: DocumentDetectionResult;
+}
 
 @Component({
   selector: 'app-image-results',
@@ -10,8 +17,9 @@ import { Image } from 'scanbot-web-sdk/@types';
 export class ImageResultsComponent implements OnInit {
 
   private scanbot = inject(ScanbotService);
+  private toast = inject(ToastService);
 
-  IMAGES: string[] = [];
+  images: DisplayImage[] = [];
 
   async ngOnInit() {
     const sdk = await this.scanbot.getSdk();
@@ -19,20 +27,23 @@ export class ImageResultsComponent implements OnInit {
     console.log("Cropped detection results:", results);
 
     results.forEach((item) => {
+
       sdk.storage.getCroppedDetectionResultImage(item.id).then(async (images) => {
         if (images && images.length > 0) {
           const original = images.find(i => i.documentId === item.id && i.type === "original");
           const cropped = images.find(i => i.documentId === item.id && i.type === "cropped");
 
-          let image: Image;
+          let image: DisplayImage = new DisplayImage();
           if (cropped) {
-            image = ScanbotSDK.Config.Image.fromWrappedImage(cropped.data);
+            image.source = ScanbotSDK.Config.Image.fromWrappedImage(cropped.data);
           } else {
-            image = ScanbotSDK.Config.Image.fromWrappedImage(original!.data);
+            image.source = ScanbotSDK.Config.Image.fromWrappedImage(original!.data);
           }
-          const jpeg = await image.toJpeg();
-          const base64 = await this.toDataUrl(jpeg);
-          this.IMAGES.push(base64);
+          const jpeg = await image.source.toJpeg();
+          image.base64 = await this.toDataUrl(jpeg);
+          image.detectionResult = item.result.detectionResult;
+
+          this.images.push(image);
         } else {
           console.warn("No images found for item:", item.id);
         }
@@ -48,5 +59,10 @@ export class ImageResultsComponent implements OnInit {
       reader.onerror = () => reject(reader.error);
       reader.readAsDataURL(blob);
     });
+  }
+
+  onImageClick(image: DisplayImage) {
+    const scores = image.detectionResult?.detectionScores;
+    this.toast.show(JSON.stringify(scores));
   }
 }
