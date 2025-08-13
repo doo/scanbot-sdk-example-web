cbd9ac0c006c71ceb85219dcb7b09b249537d2e3
Aare Undo <aare.undo@gmail.com>
2025-08-13 17:48:27 +0300

    snippets: add editor header

--- rtu-ui-barcode/src/config/TinyBarcodes.ts
+++ rtu-ui-barcode/src/config/TinyBarcodes.ts
@@ -1,3 +1,10 @@
+/**
+ * This code snippet is to be used only as a part of the website documentation.
+ * It is not intended for any use outside of the support of documentation by Scanbot SDK GmbH employees.
+ *
+ * For maintainers: whenever changing this code, ensure that links using it are still pointing to valid lines!
+ */
+
 import { UIConfig } from "scanbot-web-sdk/@types";
 
 export function applyTinyBarcodeConfig(config?: UIConfig.BarcodeScannerScreenConfiguration) {
