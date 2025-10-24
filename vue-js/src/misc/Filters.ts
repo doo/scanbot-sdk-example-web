688514398e385dad4ee76729847399e3589e2355
Aare Undo <aare.undo@gmail.com>
2025-10-24 11:41:08 +0300

    vue.js: rebuild store logic on singleton pattern (#120)
    * vue.js: rebuild store logic on singleton pattern
    
    * linter fix for vscode
    
    * vue: reload after filter change
    
    * vue: properly save image after crop apply

--- vue-js/src/misc/Filters.ts
+++ vue-js/src/misc/Filters.ts
@@ -3,15 +3,16 @@ import type { Image, ParametricFilter } from "scanbot-web-sdk/@types";
 import { toRaw } from "vue";
 
 export class Filters {
-  public static availableFilters = 
+  public static availableFilters =
     ["none", "ScanbotBinarizationFilter", "GrayscaleFilter", "ContrastFilter", "ColorDocumentFilter"] as const;
 
   public static async applyFilter(sdk: ScanbotSDK, image: Image, name: Exclude<typeof Filters.availableFilters[number], "none">) {
+
     const filter: ParametricFilter = new ScanbotSDK.Config[name]();
-    
+
     const processor = await sdk.createImageProcessor();
     const response = await processor.applyFilter(toRaw(image), filter);
-    
+
     return response.result;
   }
 }
