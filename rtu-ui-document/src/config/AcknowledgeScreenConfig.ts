70cb30df3b6d60240ee353d571dcef4c10eaaf27
Aare Undo <aare.undo@apryse.com>
2026-05-14 13:41:28 +0300

    update all examples to v9

--- rtu-ui-document/src/config/AcknowledgeScreenConfig.ts
+++ rtu-ui-document/src/config/AcknowledgeScreenConfig.ts
@@ -17,22 +17,18 @@ export function applyAcknowledgeScreenConfig(config: UIConfig.DocumentScanningFl
     // - `NONE`: Skips the quality check entirely.
     acknowledgeScreenConfig.acknowledgementMode = "ALWAYS";
 
-    // Set the minimum acceptable document quality.
-    // Options: excellent, good, reasonable, poor, very_poor, or no_document.
-    acknowledgeScreenConfig.minimumQuality = "REASONABLE";
-
     // Set the background color for the acknowledgment screen.
     acknowledgeScreenConfig.backgroundColor = "#EFEFEF";
 
     // You can also configure the buttons in the bottom bar of the acknowledgment screen,
     // e.g. to force the user to retake, if the captured document is not OK.
-    acknowledgeScreenConfig.bottomBar.acceptWhenNotOkButton.visible = false;
+    acknowledgeScreenConfig.bottomBar.proceedAnywayButton.visible = false;
 
     // Hide the titles of the buttons.
-    acknowledgeScreenConfig.bottomBar.acceptWhenNotOkButton.title.visible = false;
-    acknowledgeScreenConfig.bottomBar.acceptWhenOkButton.title.visible = false;
+    acknowledgeScreenConfig.bottomBar.acceptWhenAcceptableButton.title.visible = false;
+    acknowledgeScreenConfig.bottomBar.proceedAnywayButton.documentNotFound.visible = false;
     acknowledgeScreenConfig.bottomBar.retakeButton.title.visible = false;
 
     // Configure the acknowledgment screen's hint message which is shown if the least acceptable quality is not met.
-    acknowledgeScreenConfig.badImageHint.visible = true;
+    acknowledgeScreenConfig.uncertainQualityWarning.visible = true;
 }
