fc27536d3d20d242989cc3727cfae2e838286b20
Aare Undo <aare.undo@gmail.com>
2026-09-22 11:09:35 +0300

    react: allow unused variables

--- react-js/src/service/ImageUtils.ts
+++ react-js/src/service/ImageUtils.ts
@@ -98,7 +98,6 @@ export default class ImageUtils {
         });
     }
 
-    // @ts-expect-error Temporarily unused, but important convenience function
     private static uuidv4(): string {
         return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
             (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
